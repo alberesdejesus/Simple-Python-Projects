4ba78dd861038c5b5b5e47d1eeb9670a50b6f2c0
Alberes de Jesus <alberes.dejesus@hotmail.com>
2021-07-10 23:16:58 -0300

    Pequenas melhorias nas dimensões

--- TurtleCrossing/scoreboard.py
+++ TurtleCrossing/scoreboard.py
@@ -7,18 +7,18 @@ class Scoreboard(Turtle):
         super().__init__()
         self.penup()
         self.hideturtle()
-        self.score = 0
+        self.level = 1
+        self.goto(-280, 260)
         self.update_scoreboard()
 
     def update_scoreboard(self):
         self.clear()
-        self.goto(-260, 260)
-        self.write(f"Level: {self.score + 1}", align="center", font=FONT)
+        self.write(f"Level: {self.level}", align="left", font=FONT)
 
     def point(self):
-        self.score += 1
+        self.level += 1
         self.update_scoreboard()
 
-    def gameover(self):
+    def game_over(self):
         self.goto(0, 0)
-        self.write("Game Over", align="center", font=FONT)
+        self.write("GAME OVER", align="center", font=FONT)
